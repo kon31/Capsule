44faa82c1c98618056a7d67d86c138d70f678695
kon31# <ma-ko3110@outlook.jp>
2024-01-28 18:49:20 +0900

    Capsule 1/28-3

--- src/jp/co/diworks/action/Capsule.java
+++ src/jp/co/diworks/action/Capsule.java
@@ -4,6 +4,9 @@ public class Capsule {
 
 	public static void main(String[] args) {
 		Person taro = new Person("山田太郎" ,20);
-		System.out.println(taro.name); //資料()演習課題②taro.name()←必要なのか？
+		
+		System.out.println(taro.getName());
+		taro.setName("花子");
+		System.out.println(taro.getName()); //資料()演習課題②taro.name()←必要なのか？
 	}
 }
--- src/jp/co/diworks/action/Person.java
+++ src/jp/co/diworks/action/Person.java
@@ -1,15 +1,17 @@
 package jp.co.diworks.action;
 
 public class Person {
-	public String name =null;
-	public int age = 0;
+//	public String name =null;
+	private String name =null;
+//	public int age = 0;
+	private int age = 0;
 	
 	public Person (String name,int age) {
 		this.name = name;
 		this.age = age;
 	}
 	
-	public String setName() {
+	public String getName() {
 		return this.name;
 	}
 	
